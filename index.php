0624e0c8b2c6bcd31a86882e0cc284febe023dd1
matejplastiak <matejplastiak@gmail.com>
2026-03-17 12:43:18 +0100

    prvý commit
    commit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prva</title>
+</head>
+<body>
 <?php
 echo "<h1>Hello, World!</h1>";
+echo "Dnes je " . date("d.m.Y") . ".";
+echo "Aktualny cas je " . date("H:i:s") . ".";
 ?>
+</body>
+</html>
